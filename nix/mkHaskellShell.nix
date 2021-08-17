a9ff1c8351215f746d11ab00c7da59fa37ef7379
Philipp Dargel <chisui.pd@gmail.com>
2021-08-17 13:28:26 +0200

    initial

---
+++ nix/mkHaskellShell.nix
@@ -0,0 +1,34 @@
+{ pkgs ? import ./nixpkgs.pinned.nix
+}: drv: with drv.hPkgs; let
+  name = drv.name;
+  ghc = ghcWithPackages (pkgs: (drv.propagatedBuildInputs ++ drv.buildInputs));
+  cabalShim = pkgs.writeScriptBin "cabal" ''
+    cleanup() {
+      rm -f ${name}.cabal
+    }
+    trap cleanup SIGINT
+    ${hpack}/bin/hpack && PATH=${ghc}/bin:$PATH ${cabal-install}/bin/cabal $@
+    cleanup
+  '';
+  repl = pkgs.writeScriptBin "repl" ''
+    ${cabalShim}/bin/cabal repl $@
+  '';
+  watch = pkgs.writeScriptBin "watch" ''
+    ${pkgs.ghcid}/bin/ghcid -c "${repl}/bin/repl $@"
+  '';
+  run = pkgs.writeScriptBin "run" ''
+    ${cabalShim}/bin/cabal v2-run
+  '';
+in pkgs.mkShell rec {
+  inherit name;
+  buildInputs = [
+    cabalShim
+    repl
+    watch
+    run
+    haskell-language-server
+  ];
+  HISTFILE = toString ../.history;
+  LOCAL_HISTFILE = HISTFILE;
+}
+
---
+++ nix/nixpkgs.pinned.nix
@@ -0,0 +1,5 @@
+import (builtins.fetchTarball {
+  url    = "https://github.com/NixOS/nixpkgs/archive/e25bfe513f0b43babe1d52790adda137137d426b.tar.gz";
+  sha256 = "10ypvzxjhlhadwnzlhz1n5bh2ijnqybj932z57izr9py2ld56akk";
+}) {}
+
